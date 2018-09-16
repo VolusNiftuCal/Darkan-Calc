b74406d445c32a58a5c909814706c864357d74b4
Niftu <42500681+VolusNiftuCal@users.noreply.github.com>
2018-09-16 19:58:03 -0300

    XP/LVL input; Player data

--- javascript.js
+++ javascript.js
@@ -1,3 +1,26 @@
+/* Format the number to make it easier to read
+    ex: 1000 >= 1.000
+        1000000 >= 1.000.000
+*/
+function formatNumber(n) {
+    return n.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1.");
+}
+
+function newElement(element, args={}) {
+    var elem = document.createElement(element);
+    if (args.id) { elem.id = args.id };
+    if (args.className) { elem.className = args.className };
+    return elem;
+}
+
+// Removes all children from node
+function clearChildren(node) {
+    while (node.firstChild) {
+        node.removeChild(node.firstChild);
+    }
+}
+
+
 skill_names = [ 'Attack', 'Defence', 'Strength', 'Hitpoints', 'Ranged', 'Prayer', 'Magic', 'Cooking', 'Woodcutting', 'Fletching', 'Fishing', 'Firemaking', 'Crafting', 'Smithing', 'Mining', 'Herblore', 'Agility', 'Thieving', 'Slayer', 'Farming', 'Runecrafting', 'Hunter', 'Construction', 'Summoning', 'Dungeoneering'];
 
 var skill_items = new Array(25);
@@ -34,7 +57,16 @@ function SkillCalc(container) {
     this.skill = undefined;
     this.container = container;
     this.player = undefined;
+    this.playerName = undefined;
     this.multiplier = 1;
+    this.skillId = -1;
+    this.category = 'All';
+
+    this.curLvl = 1;
+    this.curXp = 0;
+    this.targetLvl = 2;
+    this.targetXp = 83;
+    this.xpDelta = this.targetXp - this.curXp;
 
     var sc = this;
     this.fetchPlayer = function(name) {
@@ -48,16 +80,19 @@ function SkillCalc(container) {
                     response.json().then(function(data) {
                         console.log('Data successfully retrieved!');
                         sc.player = data;
+                        sc.playerName = name;
                         sc.setupPlayerStats(sc.player);
+                        submit_user.removeAttribute('disabled');
+                        submit_user.value = 'Get Stats';
                     });
                 }
         }).catch(function(err){
             console.log('Failed to fetch data; Error: ' + err);
-            sc.loadError(' Error: ' + err);
+            sc.loadError('Error: ' + err);
         });
     };
     this.loadError = function(error) {
-
+        window.alert('Failed to load Player Data; [' + error + ']');
     };
 
     this.setupPlayerStats = function(player) {
@@ -69,17 +104,110 @@ function SkillCalc(container) {
         } else {
             sc.multiplier = 1;
         }
+        sc.curLvl = player.stats.skills[sc.skillId].level;
+        sc.curXp = player.stats.skills[sc.skillId].xp;
+        sc.targetLvl = player.stats.skills[sc.skillId].level + 1;
+        sc.targetXp = xpAtLevel(player.stats.skills[sc.skillId].level + 1);
+        xp_multiplier.value = sc.multiplier;
+        sc.updateData();
     };
 
-    this.display = function(skillId) {
+    this.setup = function(skillId) {
         document.getElementById('calc_title').textContent = skill_names[skillId] + ' Calculator';
-        console.log(skill_items[skillId]);
-    }
+        document.getElementById('calc_skills').style.display = 'none';
+        document.getElementById('calc').style.display = 'block';
+        sc.skillId = skillId;
+        sc.skill = skill_items[skillId];
+        if (sc.player) {
+            sc.setupPlayerStats(sc.player);
+        }
+        var category_title = undefined;
+        var category_data = undefined;
+        switch (skillId) {
+            // Magic
+            case 6:
+                category_title = [' Spellbook', 'Spell' ];
+                break;
+        }
+        if (category_title) {
+        }
+    };
+
+    this.display = function(cont) {
+    };
+
+    this.reset = function() {
+        document.getElementById('calc_title').textContent = 'Darkan Calculator';
+        document.getElementById('calc').style.display = 'none';
+        document.getElementById('calc_skills').style.display = 'block';
+        sc.skillId = -1;
+        sc.skill = undefined;
+    };
+
+    this.updateData = function() {
+        if (sc.targetLvl <= sc.curLvl && sc.targetLvl != 120) {
+            sc.targetLvl = sc.curLvl;
+            if (sc.curLvl < 120) {
+                sc.targetLvl += 1;
+            }
+            sc.targetXp = xpAtLevel(sc.targetLvl);
+        }
+        if (sc.targetXp <= sc.curXp) {
+            sc.targetXp = sc.curXp + 1;
+        }
+        sc.xpDelta = sc.targetXp - sc.curXp;
+        xp_delta.textContent = formatNumber(sc.xpDelta);
+        cur_lvl.value = sc.curLvl;
+        cur_xp.value = sc.curXp;
+        target_lvl.value = sc.targetLvl;
+        target_xp.value = sc.targetXp;
+    };
 };
 
-var skillCalc = undefined
+var skillCalc = undefined;
 window.onload = function() {
-    skillCalc = new SkillCalc(undefined);
-    calc_loading.style.display = 'none';
-    calc_skills.style.display = 'block';
+    skillCalc = new SkillCalc(document.getElementById('calc'));
+    document.getElementById('calc_loading').style.display = 'none';
+    document.getElementById('calc_skills').style.display = 'block';
+
+    cur_lvl.onchange = function() {
+        if (this.value > 120) {
+            this.value = 120;
+        }
+        skillCalc.curLvl = parseInt(this.value);
+        skillCalc.curXp = xpAtLevel(parseInt(this.value));
+        skillCalc.updateData();
+    };
+    cur_xp.onchange = function() {
+        if (this.value > 200000000) {
+            this.value = 200000000;
+        }
+        skillCalc.curXp = parseInt(this.value);
+        skillCalc.curLvl = levelAtXP(parseInt(this.value));
+        skillCalc.updateData();
+    };
+
+    target_lvl.onchange = function() {
+        if (this.value > 120) {
+            this.value = 120;
+        }
+        skillCalc.targetLvl = parseInt(this.value);
+        skillCalc.targetXp = xpAtLevel(parseInt(this.value));
+        skillCalc.updateData();
+    };
+    target_xp.onchange = function() {
+        if (this.value > 200000000) {
+            this.value = 200000000;
+        }
+        skillCalc.targetXp = parseInt(this.value);
+        skillCalc.targetLvl = levelAtXP(parseInt(this.value));
+        skillCalc.updateData();
+    };
+};
+
+function getData() {
+    var name = document.getElementById('username').value.toLowerCase().replace(/ /g, '_');
+    skillCalc.fetchPlayer(name);
+    submit_user.setAttribute('disabled', '');
+    submit_user.value = 'Loading...';
 }
--- skills/woodcutting.js
+++ skills/woodcutting.js
@@ -1,2 +1,15 @@
 skill_items[8] = [
+    new SkillItem('Regular Tree', 1, 25),
+    new SkillItem('Oak Tree', 15, 37.5),
+    new SkillItem('Willow Tree', 30, 67.5),
+    new SkillItem('Teak Tree', 35, 85),
+    new SkillItem('Maple Tree', 45, 100),
+    new SkillItem('Mahogany Tree', 50, 125),
+    new SkillItem('Arctic Pine', 54, 140.2),
+    new SkillItem('Eucalyptus', 58, 165),
+    new SkillItem('Yew Tree', 60, 175),
+    new SkillItem('Ivy', 68, 332.5),
+    new SkillItem('Magic Tree', 75, 250),
+    new SkillItem('Blisterwood Tree', 76, 200),
+    new SkillItem('Mutated Vine', 83, 140),
 ];
