00377bdfa57e17491c80fe1c6b0b9e48aab96705
Niftu <42500681+VolusNiftuCal@users.noreply.github.com>
2018-09-17 22:29:55 -0300

    Firemaking Data

--- skills/firemaking.js
+++ skills/firemaking.js
@@ -1,1 +1,16 @@
-skill_info[11] = {};
+skill_info[11] = {
+    name: 'Firemaking',
+    items: [
+        new SkillItem('Regular Logs', 1, 40),
+        new SkillItem('Oak Logs', 15, 60),
+        new SkillItem('Willow Logs', 30, 90),
+        new SkillItem('Teak Logs', 35, 105),
+        new SkillItem('Arctic Pine Logs', 42, 125),
+        new SkillItem('Maple Logs', 45, 135),
+        new SkillItem('Mahogany Logs', 50, 157.5),
+        new SkillItem('Eucalyptus Logs', 58, 193.5),
+        new SkillItem('Yew Logs', 60, 202.5),
+        new SkillItem('Magic Logs', 75, 303.8),
+        new SkillItem('Blisterwood Logs', 76, 303.8),
+    ]
+};
