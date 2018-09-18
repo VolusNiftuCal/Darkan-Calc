34d5927be027ea1f9c9036eb805c51652aec3b77
Niftu <42500681+VolusNiftuCal@users.noreply.github.com>
2018-09-17 22:30:11 -0300

    Fishing Data

--- skills/fishing.js
+++ skills/fishing.js
@@ -1,1 +1,42 @@
-skill_info[10] = {};
+skill_info[10] = {
+    name: 'Fishing',
+    categories: {
+        title: 'Category',
+        values: ['Gloves', 'Barbarian']
+    },
+    items: [
+        new SkillItem('Shrimp', 1, 10),
+        new SkillItem('Crayfish', 1, 10),
+        new SkillItem('Karambwanji', 5, 5),
+        new SkillItem('Sardine', 5, 20),
+        new SkillItem('Herring', 10, 30),
+        new SkillItem('Anchovy', 15, 40),
+        new SkillItem('Mackerel', 16, 20),
+        new SkillItem('Trout', 20, 50),
+        new SkillItem('Cod', 23, 45),
+        new SkillItem('Pike', 25, 60),
+        new SkillItem('Slimey Eel', 28, 65),
+        new SkillItem('Salmon', 30, 70),
+        new SkillItem('Frog Spawn', 33, 75),
+        new SkillItem('Tuna', 35, 80),
+        new SkillItem('Cave Eel', 38, 80),
+        new SkillItem('Rainbowfish', 38, 80),
+        new SkillItem('Lobster', 40, 90),
+        new SkillItem('Bass', 46, 100),
+        new SkillItem('Leaping Trout', 48, 50),
+        new SkillItem('Swordfish', 50, 100),
+        new SkillItem('Lava Eel', 53, 60),
+        new SkillItem('Tuna (Barbarian)', 55, 80, ['Barbarian']),
+        new SkillItem('Leaping Salmon', 58, 70),
+        new SkillItem('Monkfish', 62, 120),
+        new SkillItem('Karambwan', 65, 105),
+        new SkillItem('Swordfish (Gloves)', 65, 200, ['Gloves']),
+        new SkillItem('Leaping Sturgeon', 70, 80),
+        new SkillItem('Swordfish (Barbarian)', 70, 100, ['Barbarian']),
+        new SkillItem('Shark', 76, 110),
+        new SkillItem('Cavefish', 85, 300),
+        new SkillItem('Shark (Gloves)', 90, 210, ['Gloves']),
+        new SkillItem('Rocktail', 90, 380),
+        new SkillItem('Shark (Barbarian)', 96, 130, ['Barbarian']),
+    ]
+};
