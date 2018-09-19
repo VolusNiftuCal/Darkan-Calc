5dba387ebe8703012f4f546836ce42747b62cf09
Niftu <42500681+VolusNiftuCal@users.noreply.github.com>
2018-09-19 16:00:05 -0300

    Agility Data

--- skills/agility.js
+++ skills/agility.js
@@ -1,1 +1,11 @@
-skill_info[16] = {};
+skill_info[16] = {
+    name: 'Agility',
+    items: [
+        new SkillItem('Gnome Course', 1, 86.5),
+        new SkillItem('Agility Pyramid', 30, 1622),
+        new SkillItem('Barbarian Course', 35, 155.8),
+        new SkillItem('Wilderness Course', 52, 571.5),
+        new SkillItem('Advanced Gnome Course', 85, 725),
+        new SkillItem('Advanced Barbarian', 90, 751.7),
+    ]
+};
