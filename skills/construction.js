9ebea4a94d509440ad2268da2fac94a34a4c0db4
Niftu <42500681+VolusNiftuCal@users.noreply.github.com>
2018-09-19 16:00:14 -0300

    Construction Data

--- skills/construction.js
+++ skills/construction.js
@@ -1,1 +1,15 @@
-skill_info[22] = {};
+skill_info[22] = {
+    name: 'Construction',
+    items: [
+        // Materials
+        new SkillItem('Plank + Nail', 1, 30.5, ['Materials']),
+        new SkillItem('Cloth', 2, 15, ['Materials']),
+        new SkillItem('Clay', 3, 10, ['Materials']),
+        new SkillItem('Iron Bar', 5, 10, ['Materials']),
+        new SkillItem('Limestone Brick', 5, 20, ['Materials']),
+        new SkillItem('Steel Bar', 7, 20, ['Materials']),
+        new SkillItem('Oak Plank', 15, 60, ['Materials']),
+        new SkillItem('Teak Plank', 35, 90, ['Materials']),
+        new SkillItem('Mahogany Plank', 40, 140, ['Materials']),
+    ]
+};
